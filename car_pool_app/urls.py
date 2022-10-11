d09525a5e33b55de4f9279c06763323bba4c6e54
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-10-11 14:19:26 +0300

    done

--- car_pool_app/urls.py
+++ car_pool_app/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),  
+    path('', views.index), 
+    path('proce-login',views.proc_login) ,
+    path('success',views.success),
+    path('registration/',views.registration ),
+    path('regist_proc',views.regist_proc),
+    path('wall',views.wall),
     path('trips/', views.show_detials_trip),
     path('my-trips/', views.my_trip),
 ]
